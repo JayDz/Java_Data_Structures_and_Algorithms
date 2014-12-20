6cd776cdefeae7939011f8a82644a7324196e6ff
Jeffrey <jeffreydiaz27@gmail.com>
2014-12-20 00:06:47 -0500

    Implementation of the classic Linear Search algorithms for arrays and iterable collections.

---
+++ src/com/jeffreydiaz/search/LinearSearch.java
@@ -0,0 +1,40 @@
+package com.jeffreydiaz.search;
+
+/**
+ * Implementation of the classic Linear Search algorithm.
+ * @author Jeffrey Diaz
+ */
+public class LinearSearch {
+
+	/**
+	 * Linearly search for item elem in array items.
+	 * @param items an array of Item objects who've implemented the equals method.
+	 * @param elem item to search array items for.
+	 * @return true if elem is found within items.
+	 */
+	public static <Item> boolean search(Item[] items, Item elem)
+	{
+		for (int i = 0; i < items.length; ++i) {
+			if (items[i].equals(elem)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Linearly search for elem in Iterable items.
+	 * @param items Collection implementing the Iterable interface.
+	 * @param elem Item to search collection for.
+	 * @return true if elem is found within items.
+	 */
+	public static <Item> boolean search(Iterable<Item> items, Item elem)
+	{
+		for (Item item : items) {
+			if (item.equals(elem)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
