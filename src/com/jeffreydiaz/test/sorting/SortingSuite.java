6d7db7c930b910d20f4d1ae7d9f7acfd3a4611a5
Jeffrey <jeffreydiaz27@gmail.com>
2014-12-25 21:29:39 -0500

    Junit Suite for the sorting package.

---
+++ src/com/jeffreydiaz/test/sorting/SortingSuite.java
@@ -0,0 +1,12 @@
+package com.jeffreydiaz.test.sorting;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * A JUNIT Suite for the sorting package.
+ * @author Jeffrey Diaz
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BubbleTest.class, InsertionTest.class, SelectionTest.class})
+public class SortingSuite {}
