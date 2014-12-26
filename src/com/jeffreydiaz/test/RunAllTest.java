d903c488be8e3d1fb087fa846385612863b3a696
Jeffrey <jeffreydiaz27@gmail.com>
2014-12-25 21:29:55 -0500

    Added the SortingSuite to list.

--- src/com/jeffreydiaz/test/RunAllTest.java
+++ src/com/jeffreydiaz/test/RunAllTest.java
@@ -3,6 +3,7 @@ package com.jeffreydiaz.test;
 import com.jeffreydiaz.test.collections.CollectionsSuite;
 import com.jeffreydiaz.test.connectivity.ConnectivitySuite;
 import com.jeffreydiaz.test.search.SearchSuite;
+import com.jeffreydiaz.test.sorting.SortingSuite;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -11,5 +12,8 @@ import org.junit.runners.Suite;
  * Suite which runs all Test Cases in project.
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({CollectionsSuite.class, ConnectivitySuite.class, SearchSuite.class})
+@Suite.SuiteClasses({CollectionsSuite.class,
+		     ConnectivitySuite.class,
+		     SearchSuite.class,
+	             SortingSuite.class})
 public class RunAllTest {}
