1b485e3ad150ae67a01723e21d06bb01ab36b6ab
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2014-12-29 01:40:22 -0500

    Implementation of a Priority Queue where the max values are removed.

---
+++ src/com/jeffreydiaz/collections/OrderedMaxPQ.java
@@ -0,0 +1,39 @@
+package com.jeffreydiaz.collections;
+
+/**
+ * Implementation of a Priority Queue with an array as the internal data structure.
+ * Operation cost:
+ *      remove: ~1 (however, we may need to shrink array).
+ *      insert: ~N
+ *      @author Jeffrey Diaz
+ */
+public class OrderedMaxPQ<Key extends Comparable<Key>> extends AbstractOrderedPQ<Key> {
+
+	/**
+	 * Create an empty OrderedMaxPQ with an initial capacity of 8.
+	 */
+	public OrderedMaxPQ()
+	{
+		super();
+	}
+
+	/**
+	 * Create an empty OrderedMaxPQ with an initial capacity.
+	 * @param capacity initial capacity.
+	 */
+	@SuppressWarnings("unchecked")
+	public OrderedMaxPQ(int capacity)
+	{
+		super(capacity);
+	}
+
+	@Override
+	public void orderKeys()
+	{
+		for (int i = size - 1; i > 0; --i) {
+			if (less(keys[i], keys[i-1])) {
+				exch(i,i-1);
+			}
+		}
+	}
+}
